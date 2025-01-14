854870804c660bddaa51afbb2fe7e8d9fe972b65
affan14 <affan.ahmad@yahoo.de>
2025-01-14 01:44:41 +0100

    Added files

---
+++ vite-project/eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended, eslintConfigPrettier,
+  {
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "prefer-const": "error",
+      "eqeqeq": "error",
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "no-var": "error",
+      "curly": "error",
+      "camelcase": "error",
+    }
+  }
+];
