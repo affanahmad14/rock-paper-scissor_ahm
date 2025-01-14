c0a9668be50cb8dbcc84ac37e544701e6007b0ed
affan14 <affan.ahmad@yahoo.de>
2025-01-14 17:58:41 +0100

    Editing html file

--- vite-project/eslint.config.js
+++ vite-project/eslint.config.js
@@ -2,23 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended, eslintConfigPrettier,
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {
       "no-console": "off",
       "no-unused-vars": "off",
       "no-undef": "off",
       "prefer-const": "error",
-      "eqeqeq": "error",
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"],
+      eqeqeq: "error",
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
       "no-var": "error",
-      "curly": "error",
-      "camelcase": "error",
-    }
-  }
+      curly: "error",
+      camelcase: "error",
+    },
+  },
 ];
